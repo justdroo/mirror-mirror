79975fcc35a20a5fb70dc3902538b44e2de9c2e2
Drew Nickerson <dnick13@gmail.com>
2018-02-12 11:28:07 -0500

    Added 01 answer lab and made lab work in terminal

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source "https://rubygems.org"
 
 gem 'rspec'
 gem 'nyan-cat-formatter'
-gem 'sinatra'
---
+++ src/labs/01_data_types_and_variables/runner.rb
@@ -0,0 +1,24 @@
+require_relative './01_data_types_and_variables'
+
+madLib = Lab.new()
+
+name = madLib.return_a_string
+age = madLib.return_an_integer
+
+adjectives = madLib.return_an_array
+last_adjective = adjectives.pop
+adj_string = adjectives.join(", ") + " and " + last_adjective
+
+favorite_things = madLib.return_a_hash
+favorites = favorite_things.map { |key, value| "My favorite #{key} is #{value}." }
+all_favorites = favorites.join(" ")
+
+if madLib.return_true == true
+  i_am = "I am an excellent programmer, "
+end
+
+if madLib.return_false == false
+  i_am_not = "and that is not fake news!"
+end
+
+puts "Hello World! My name is #{name}. I am #{age} years old and I am #{adj_string}. #{all_favorites} #{i_am}#{i_am_not}"
