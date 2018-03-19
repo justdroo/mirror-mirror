cb122442c086b407406eebe8bdb02f961c564395
Drew Nickerson <dnick13@gmail.com>
2018-03-19 13:59:43 -0400

    Creating lecture notes as ruby files

---
+++ src/lecture/02_methods/methods_02_arguments.rb
@@ -0,0 +1,9 @@
+def sayHiTo(name)
+  puts "Hello #{name}"
+  puts "Hello " + name
+end
+
+sayHiTo("Dr. Oo")
+# Hello Dr. Oo
+# Hello Dr. Oo
+# => nil
---
+++ src/lecture/02_methods/methods_07_return_statement.rb
@@ -0,0 +1,12 @@
+def its_up_to_jack
+  "No"
+end
+
+def could_jack_and_rose_both_fit_on_the_door?
+  puts "Could #{jack} and #{rose} both have fit on the door?"
+  return its_up_to_jack
+  "Yes"
+end
+
+could_jack_and_rose_both_fit_on_the_door
+"No"
